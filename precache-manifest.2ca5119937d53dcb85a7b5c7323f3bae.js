55011a444136182f0d0fc53414e5b47b16051551
RiBpy <77004199+RiBpy@users.noreply.github.com>
2022-08-22 21:13:51 +0600

    Updates

---
+++ precache-manifest.2ca5119937d53dcb85a7b5c7323f3bae.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1de303e73eca672c92709a7366fee9d",
+    "url": "/react_stripe/index.html"
+  },
+  {
+    "revision": "9006f31ea5062af66f3f",
+    "url": "/react_stripe/static/css/main.1e600e6c.chunk.css"
+  },
+  {
+    "revision": "fdaa84bd64399522b9cd",
+    "url": "/react_stripe/static/js/2.415be811.chunk.js"
+  },
+  {
+    "revision": "f8d7b1ec4b9bebad776926ca905fbc24",
+    "url": "/react_stripe/static/js/2.415be811.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9006f31ea5062af66f3f",
+    "url": "/react_stripe/static/js/main.aca55afa.chunk.js"
+  },
+  {
+    "revision": "7e2ec34b8536f52d6a45",
+    "url": "/react_stripe/static/js/runtime-main.53174de9.js"
+  },
+  {
+    "revision": "882a211ad0bae36b9c11715c9291f36b",
+    "url": "/react_stripe/static/media/hero.882a211a.svg"
+  },
+  {
+    "revision": "86d618b5be1508bcbbb31d6393f089f6",
+    "url": "/react_stripe/static/media/logo.86d618b5.svg"
+  },
+  {
+    "revision": "e908706c41907a9611ba9c4d61a32f7c",
+    "url": "/react_stripe/static/media/phone.e908706c.svg"
+  }
+]);
